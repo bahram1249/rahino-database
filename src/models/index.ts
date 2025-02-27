6f81f43446a23c79d4ee4e8963c130496fbe9678
bahram <bahram.rajabiws@gmail.com>
2025-02-27 20:18:22 +0330

    remove models

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,7 +1,1 @@
 export * from "./core";
-export * from "./discount-coffe";
-export * from "./eav";
-export * from "./ecommerce-eav";
-export * from "./pcm";
-export * from "./bpmn";
-export * from "./guarantee";
--- src/subsystem-models/index.ts
+++ src/subsystem-models/index.ts
@@ -1,7 +1,1 @@
 export * from "./core";
-export * from "./eav";
-export * from "./ecommerce";
-export * from "./pcm";
-export * from "./discount-coffe";
-export * from "./bpmn";
-export * from "./guarantee";
